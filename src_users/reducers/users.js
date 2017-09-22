b0797ae4b401b72463400aca81f10d69bbd2b1ad
Mike Turley <mturley@hpe.com>
2017-09-22 14:15:00 -0400

    One more eslint fix

--- src_users/reducers/users.js
+++ src_users/reducers/users.js
@@ -12,15 +12,15 @@ export default function users(state = {}, action) {
       return action.users;
 
     case USERS_ADD_SAVE:
-      const user = action.user;
-      user.id = user.id || Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-      return [...state, user];
+      return [...state, {
+        ...action.user,
+        id: (action.user.id || Math.floor(Math.random() * ((9999 - 1000) + 1)) + 1000),
+      }];
 
     case USERS_EDIT_SAVE:
       return state.map(user =>
         (Number(user.id) === Number(action.user.id) ? { ...action.user } : user),
       );
-      break;
 
     case USERS_DELETE_SAVE:
       return state.filter(user =>
