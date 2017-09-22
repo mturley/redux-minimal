1923a3d59761e4f77d55316369457254df4cd5e1
Mike Turley <mturley@hpe.com>
2017-09-22 13:11:01 -0400

    Manual changes to pass lint

--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
-import { history } from './store.js';
+import { history } from './store';
 import App from './components/App';
 import Home from './components/Home';
-import UserEdit from './components/UserEdit';
 import NotFound from './components/NotFound';
 
-// build the router
 const router = (
   <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path="user-edit(/:id)" component={UserEdit} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
 );
 
-// export
-export { router };
+export default router;
